99eb22df8c06b0336f8b752c55cd2161b28e2883
huangxian <2650710561@qq.com>
2020-05-26 16:03:05 +0800

    reset

--- src/index.js
+++ src/index.js
@@ -1,26 +1,12 @@
 import './scss/index.scss'
 
-import Button from './components/button/Button.vue'
-import ButtonGroup from './components/button/ButtonGroup.vue'
-import Input from './components/input/Input.vue'
 
-const components = [Button, ButtonGroup, Input].map(component => {
-  component.install = Vue => {
-    Vue.component(component.name, component)
-  }
-  return component
-})
 
 const install = Vue => {
-  components.forEach(component => {
-    Vue.component(component.name, component)
-  })
+  console.log(Vue)
 }
 
 export default {
-  install,
-  Button,
-  ButtonGroup,
-  Input
+  install
 }
 
--- src/mixins/optionReactiveHandler.js
+++
@@ -1,7 +0,0 @@
-export default {
-  watch: {
-    option () {
-      this.optionOfClass = this.genClassByOption();
-    }
-  }
-}
