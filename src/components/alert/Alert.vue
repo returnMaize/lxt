1b88701c955c49c9aa9eb5e08ed4c83e18798ea2
huangxian <2650710561@qq.com>
2020-05-28 18:27:10 +0800

    simple complete alert

--- src/components/alert/Alert.vue
+++ src/components/alert/Alert.vue
@@ -1,11 +1,41 @@
 <template>
-  <div class="lxt-alert">
-    lxt-alert
+  <div :class="mainClassArr">
+    <slot name="icon">
+      <i :class="iconClassArr"></i>
+    </slot>
+
+    <span class="lxt-alert__content">
+      <slot name="default"></slot>
+    </span>
   </div>
 </template>
 
 <script>
 export default {
-  name: "lxt-alert"
+  name: "lxt-alert",
+
+  props: {
+    type: {
+      type: String,
+      default: "primary"
+    },
+    icon: {
+      type: String
+    }
+  },
+
+  computed: {
+    mainClassArr() {
+      const arr = [];
+      const { type } = this;
+      return arr.concat(`lxt-alert__${type}`);
+    },
+    iconClassArr() {
+      const arr = ["iconfont"];
+      const { icon } = this;
+      arr.push(icon);
+      return arr.concat(icon);
+    }
+  }
 };
 </script>
