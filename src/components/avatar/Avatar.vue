2c78c3e5e2af93ced9d316353f79f80a593a6d6c
huangxian <2650710561@qq.com>
2020-06-01 10:18:50 +0800

    add Avatar component

--- src/components/avatar/Avatar.vue
+++ src/components/avatar/Avatar.vue
@@ -1,6 +1,11 @@
 <template>
-  <div :class="[`lxt-avatar__${ size }__${ color }__${ textColor }`]">
-    <slot>{{ text }}</slot>
+  <div :class="[`lxt-avatar__${ size }__${ color }`]"
+    :style="{ 
+      color: textColor, 
+      backgroundImage: `url(${src})`, 
+      backgroundSize: 'cover'
+     }">
+    <slot v-if="!src">{{ finalText }}</slot>
   </div>
 </template>
 
@@ -28,6 +33,19 @@ export default {
       type: String,
       default: "normal"
     }
+  },
+
+  computed: {
+    finalText() {
+      return this.src ? "" : this.text;
+    }
   }
 };
 </script>
+
+<style scoped>
+div {
+  background-image: url("https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQGt4EM_r-2oqcLhC7uP2o9PUKz52DB7O1Efg7JGuxk2VWJcty9&usqp=CAU");
+  background-size: cover;
+}
+</style>
